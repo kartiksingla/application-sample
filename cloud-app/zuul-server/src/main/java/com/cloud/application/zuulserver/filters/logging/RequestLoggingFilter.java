c3a85ff40f0dc46428579c88554528672d96ce23
Kartik <singla.kartik1@gmail.com>
2019-01-23 12:28:38 +0530

    Zuul server added

---
+++ cloud-app/zuul-server/src/main/java/com/cloud/application/zuulserver/filters/logging/RequestLoggingFilter.java
@@ -0,0 +1,41 @@
+package com.cloud.application.zuulserver.filters.logging;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class RequestLoggingFilter extends ZuulFilter {
+
+	private static final Logger LOG = LoggerFactory.getLogger(RequestLoggingFilter.class);
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+		String requestURI = request.getRequestURI();
+		LOG.info("{}", requestURI);
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 0;
+	}
+
+}
