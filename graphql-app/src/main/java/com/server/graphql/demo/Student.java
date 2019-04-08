d557f1d4db4e562ac0423f783a5b72e34c215756
Kartik Singla <karsingl@publicisgroupe.net>
2019-04-08 10:24:31 +0530

    Basic layout for graphql (Not completed)

---
+++ graphql-app/src/main/java/com/server/graphql/demo/Student.java
@@ -0,0 +1,14 @@
+package com.server.graphql.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String name;
+	private String passportNumber;
+}
